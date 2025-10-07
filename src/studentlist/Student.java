db46bcb363bdfaf71ee067223e3249b5b25a679f
Ishan Sapkota <ishansapkota223@gmail.com>
2025-10-07 10:02:11 -0400

    add my name

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,4 +1,4 @@
-/*
+/* IShan Sapkota
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
